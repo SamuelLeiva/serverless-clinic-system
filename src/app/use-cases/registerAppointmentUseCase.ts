73ad5a98890ef69b8b60128467bf07671b627dac
SamuelLeiva <leiva20152@gmail.com>
2025-10-05 14:51:16 -0500

    fix: idGenerator and id added as parameter in snsService.

--- src/app/use-cases/registerAppointmentUseCase.ts
+++ src/app/use-cases/registerAppointmentUseCase.ts
@@ -1,21 +1,22 @@
 import { AppointmentRequest } from '../../types/appointment';
 import { IAppointmentRepository } from '../ports/appointmentRepository';
+import { IIdGenerator } from '../ports/idGenerator';
 import { ISnsService } from '../ports/snsService';
-import { v4 as uuidv4 } from 'uuid'; 
 
 export class RegisterAppointmentUseCase {
     constructor(
         private appointmentRepository: IAppointmentRepository,
-        private snsService: ISnsService
+        private snsService: ISnsService,
+        private idGenerator: IIdGenerator
     ) {}
 
     async execute(request: AppointmentRequest): Promise<void> {
-        const appointmentId = uuidv4();
+        const appointmentId = this.idGenerator.generate();
 
         // Registrar Petición en DynamoDB
         await this.appointmentRepository.save(request, appointmentId, 'pending');
 
         // Publicar en SNS para el procesamiento asíncrono por país
-        await this.snsService.publishAppointmentRequest(request);
+        await this.snsService.publishAppointmentRequest(request, appointmentId);
     }
 }
